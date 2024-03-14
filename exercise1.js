c0b0828d61836b8ea8cbd2b5874ab2082d27018a
NormanKalyuzhny <innerspiritu@gmail.com>
2024-03-14 18:13:10 +0200

    Added 3 .js files for js_homework2 branch

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+let a = 100; // < change this to see the grade
+if (a>=90) {console.log('excellent')} 
+        else if (a>=70 && a<=89) {console.log('good')}
+        else if (a>=50 && a<=69) {console.log('fair')}
+    else  {
+console.log('bad')
+}
---
+++ exercise2.js
@@ -0,0 +1,25 @@
+//switch-case
+let a = 4;
+let b = 2;
+
+switch(0) { // 0 is for default
+    case 1:  
+      console.log(a + b);
+      break;
+
+    case 2:  
+      console.log(a - b);
+      break;
+
+    case 3:  
+      console.log(a * b);
+      break;
+
+    case 4:  
+      console.log(a / b);
+      break;          
+    
+    default:
+    console.log('nope');
+    break;
+  }
